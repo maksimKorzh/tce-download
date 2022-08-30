5f45380d166e1bd5040e182a80bbce945464b9ef
Code Monkey King <freesoft.for.people@gmail.com>
2022-08-30 19:03:48 +0300

    fixed no deps packages test cases

--- tce-download.py
+++ tce-download.py
@@ -33,8 +33,8 @@ def download(url, mode):
 def fetch(item):
   # substitute KERNEL
   if 'KERNEL' in item:
-    item = item.replace('KERNEL', '5.15.10-tinycore64')
     # for 32-bit version use '5.15.10-tinycore'
+    item = item.replace('KERNEL', '5.15.10-tinycore64')
   
   # init urls
   tcz = MIRROR + item
@@ -62,9 +62,24 @@ def fetch(item):
       items = f.read().split('\n')
       for dep_item in items[:-1] if len(items) > 1 else items:
         if dep_item != ' ':
-          if dep_item in listdir(TCE_PATH): return 1
+          if dep_item in listdir(TCE_PATH): continue
           print('Resolving dependency for "' + item + '":', dep_item)
           fetch(dep_item)
+
+def check_deps(item):
+  deps_tree = requests.get(MIRROR + item + '.tree')
+  if '<html>' in deps_tree.text: return
+  deps_tree = list(filter(None, deps_tree.text.split('\n')))
+  deps_tree = set([i.strip() for i in deps_tree])
+  deps_fetch = [
+    i for i in listdir(TCE_PATH)
+    if '.md5' not in i and '.dep' not in i
+  ]
+  for dep in deps_tree:
+    if dep not in deps_fetch:
+      print('BUG ALERT! missing dependency:', dep)
+      print('Please report the bug to "freesoft.for.people@gmail.com" if you see this message')
+      sys.exit(1)
 
 ##############################################
 #
@@ -97,6 +112,7 @@ with open('download.lst') as f: downloads = f.read().split('\n')[:-1]
 for item in downloads:
   try:
     fetch(item)
+    check_deps(item)
     with open('./tce/onboot.lst', 'a') as f:
       f.write(item + '\n')
   except Exception as e: print(e)
